283e0ba5b5ffc1a61d659151679026a05f266060
Jacob Martinez <jmartinez.data@gmail.com>
2020-02-28 09:01:51 -0600

    Revised searchByTrait, work in progress

--- app.js
+++ app.js
@@ -24,24 +24,19 @@ function app(people){
 
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
-
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
-
   if(!person){
     alert("Could not find that individual.");
     return app(people); // restart
   }
-
   var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
-
   switch(displayOption){
     case "info":
-      // TODO: get person's info
       displayPerson(person);
       break;
     case "family":
       // TODO: get person's family
-      
+
       break;
     case "descendants":
       // TODO: get person's descendants
@@ -74,9 +69,7 @@ function getSearchTraits(){
   var parameters = [];
   var searching = true;
   while (searching) {
-    var traits = promptFor("Select Search Criteria: (Enter number and choose when finished)"
-                            + "\n1. Gender\n2. Date of Birth\n3. Height\n4. Weight"
-                            + "\n5. Eye Color\n6. Occupation\n7. Finished");
+    var traits = promptFor("Select Search Criteria: (Enter number and choose when finished)\n1. Gender\n2. Date of Birth\n3. Height\n4. Weight\n5. Eye Color\n6. Occupation\n7. Finished", chars);
     if (traits == "7" || traits.toLowerCase() == "finished") {
       searching = false;
       continue;
@@ -107,31 +100,17 @@ function enterSearchTraits(search, input){
     case "6":
       search.push("occupation")
     default:
-      getPush;
+      app(people);
       break;
   }
   return search;
 }
 
 function searchByTrait(people){
-  var trait = promptFor("What trait would you like to search for?", chars);
-//map traits
-  switch(trait) {
-    case "gender":
-      break;
-    case "dob":
-      break;
-    case "height":
-      break;
-    case "weight":
-      break;
-    case "eyecolor":
-      break;
-    case "occupation":
-      break;
-    default:
-      return searchByTrait(people);
-  }
+  var traits = getSearchTraits();
+  var values = traits.map(x => promptFor("What is the person's " + x, chars));
+
+  return people.filter(x => people[traits[x]] == people[traits[values]]);
 }
 
 // alerts a list of people
@@ -154,30 +133,30 @@ function displayPerson(person){
   personInfo += "Occupation: " + person.occupation + "\n";
   alert(personInfo);
 }
-function displayDescendants(person, people){
-  
-  var members= person;
-  members.push(people.filter(function(el){
-    for (let i = 0; i < members.length; i++) {
-      if (el.parents == members[i].id) {
-        return el.
-      }
-      
-    }
-    return el.parents[0] == person.map();
-
-  }));
-  if (members.length == person.length) {
-     displayPeople(members)
-  }
-  else{
-    displayDescendants(members, people);
-  }
-  
-  
-  
-  
-}
+// function displayDescendants(person, people){
+//
+//   var members= person;
+//   members.push(people.filter(function(el){
+//     for (let i = 0; i < members.length; i++) {
+//       if (el.parents == members[i].id) {
+//         return el.
+//       };
+//
+//     }
+//     return el.parents[0] == person.map();
+//
+//   }));
+//   if (members.length == person.length) {
+//      displayPeople(members)
+//   }
+//   else{
+//     displayDescendants(members, people);
+//   }
+//
+//
+//
+//
+// }
 
 // function that prompts and validates user input
 function promptFor(question, callback){
